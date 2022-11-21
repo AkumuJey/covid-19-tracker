c3096742b085f16a748f673f53051a95137dab8f
AkumuJey <josephowino7@gmail.com>
2022-11-21 21:37:44 +0300

    First commit for covid 19 tracker

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="text-center bg-blue-400 text-white p-4 mb-10">
+    <div class="text-3xl md:text-3xl font-bold mb-3"> COVID-19 Tracker</div>
+    <p class="text-blue-800">Source of data <a href="https://covid19api.com" target="_blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return{
+      covid: require('../assets/corona.jpg')
+    }
+  }
+}
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="home" v-if="!loading">
+    <dataTitle :text="title" :dataDate="dataDate" />
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-grey-500 text-3xl mt-10 mb-6">
+      Fetching data
+    </div>
+    <img :src="loadingImage" class="w-24 mx-auto" alt="">
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import dataTitle from '../components/DataTitle.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    dataTitle
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/loading.gif')
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.Countries
+    this.loading = false
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
   }
 }
 </script>
